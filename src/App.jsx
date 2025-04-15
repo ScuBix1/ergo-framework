f19e835ea0b55498be97b091385f2f5c3363f58b
Jupower38300 <julien.legrand@my-digital-school.org>
2025-04-15 16:30:33 +0200

    add: Added React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
