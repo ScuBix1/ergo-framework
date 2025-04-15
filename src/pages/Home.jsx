b238bc9880c95254e74a4a0fc7b40bf57a02f3e3
ScuBix1 <bastian.monnin1@gmail.com>
2025-04-15 17:02:49 +0200

    select start

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,39 @@
+import Select from '../components/Select/Select';
+
+const students = [
+  {
+    firstname: 'bastian',
+    lastname: 'monnin',
+  },
+  {
+    firstname: 'bastian',
+    lastname: 'monnin',
+  },
+  {
+    firstname: 'bastian',
+    lastname: 'monnin',
+  },
+  {
+    firstname: 'bastian',
+    lastname: 'monnin',
+  },
+  {
+    firstname: 'bastian',
+    lastname: 'monnin',
+  },
+  {
+    firstname: 'bastian',
+    lastname: 'monnin',
+  },
+];
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <h1 className='text-green-400'>Home Page</h1>
+      <Select tabStudents={students} />
+    </div>
+  );
 }
 
 export default Home;
