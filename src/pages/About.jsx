b88754c70c0aa094cbeee5d11e1059003e5f8b1f
Jupower38300 <julien.legrand@my-digital-school.org>
2025-04-16 11:57:56 +0200

    add: Added Header

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
-import Button from "../components/Bouton";
+import Header from "@/components/Header";
+import { Button } from "@/components/ui/button";
 
 function About() {
   return (
     <>
+      <Header />
       <h1>About Page</h1>
-      <Button children={"Click-moi"} />
+      <Button variant="outline">Bouton</Button>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,41 @@
-import SelectName from '../components/SelectName/SelectName';
+import { Button } from "@/components/ui/button";
+import SelectName from "../components/SelectName/SelectName";
+import Header from "@/components/Header";
 
 const students = [
   {
-    firstname: 'bastian',
-    lastname: 'monnin',
+    firstname: "bastian",
+    lastname: "monnin",
   },
   {
-    firstname: 'bastian',
-    lastname: 'monnin',
+    firstname: "bastian",
+    lastname: "monnin",
   },
   {
-    firstname: 'bastian',
-    lastname: 'monnin',
+    firstname: "bastian",
+    lastname: "monnin",
   },
   {
-    firstname: 'bastian',
-    lastname: 'monnin',
+    firstname: "bastian",
+    lastname: "monnin",
   },
   {
-    firstname: 'bastian',
-    lastname: 'monnin',
+    firstname: "bastian",
+    lastname: "monnin",
   },
   {
-    firstname: 'bastian',
-    lastname: 'monnin',
+    firstname: "bastian",
+    lastname: "monnin",
   },
 ];
 
 function Home() {
   return (
     <div>
-      <h1 className='text-green-400'>Home Page</h1>
+      <Header />
+      <h1 className="text-green-400">Home Page</h1>
       <SelectName tabStudents={students} />
+      <Button variant="outline">Button</Button>
     </div>
   );
 }
